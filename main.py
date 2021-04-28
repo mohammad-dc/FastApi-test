667e436f204424c96348eb6d6fec8e2315aeac7f
Mohammad Ahmad <ma3571318z@gmail.com>
2021-04-28 14:27:59 +0300

    test api how to work

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 app = FastAPI()
 
 @app.get('/')
@@ -9,3 +12,28 @@ def index():
 @app.get('/about')
 def about():
     return {'data': 'about page'}
+
+
+@app.get('/blog/{id}')
+def blog_details(id: int):
+    return {'blog_id': id}
+
+@app.get('/blog')
+def blog(limit=10, published: bool = True, sort: Optional[str] = None):
+    # like http://localhost:8000/blog?limit=44&published=true
+    if published:  
+        return {'blog_list': f'{limit} publised blogs from DB'}
+    else:
+        return {'blog_list': f'{limit} blogs from DB'}
+
+
+# for post requests
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+    
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog is created with title as {blog.title}'}
